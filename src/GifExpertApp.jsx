8978bce52da10895f95d5f7c0a862e7f84ecc30f
jason <167580826+JasonBarrientos@users.noreply.github.com>
2025-08-08 18:56:32 -0300

    Se crea elemento gifexpert

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,7 @@
+const GifExpertApp = () => {
+  return (
+    <h1>GifExpertApp</h1>
+  )
+}
+
+export default GifExpertApp 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import GifExpertApp from './GifExpertApp'
+import './styles.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+{/* TITLE */}
+    <GifExpertApp />
+    {/* input */}
+    {/* listado de gif */}
+      {/* gif item */}
   </StrictMode>,
 )
