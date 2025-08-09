b312f9fcfc60271b13746f5a6137f550f9c2f583
jason <167580826+JasonBarrientos@users.noreply.github.com>
2025-08-09 01:59:59 -0300

    mostrar titulos

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGif } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+    const [images, setImages] = useState([]);
+
+    const getImages= async (category)=>{
+        const newImages=  await  getGif(category)
+        setImages(newImages)
+    }
 
    useEffect(() => {
-    getGif(category);
+    getImages()
    }, [])
+
+
+   
    
     return (
         <>
             <h3>{category}</h3>
+            <ol>
+                {
+                    images.map(img=>{
+                        return (
+                        <li key={img.id}>{img.title}</li>
+                    )
+                    })
+                }
+            </ol>
         </>
     )
 }
