d5b4ef46377bf74f63e6271f3f249b6c2328515a
jason <167580826+JasonBarrientos@users.noreply.github.com>
2025-09-06 14:34:33 -0300

    gif lsit

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,42 +1,18 @@
+import { GifsList } from "./gifs/GifsList"
+import { PreviousSearches } from "./gifs/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
+import { CustomHeader } from "./shared/components/CustomHeader"
+import { SearchBar } from "./shared/components/SearchBar"
 
 export const GifsApp = () => {
   return (
     <>
-      {/*Header */}
-      <div className="content-center">
-        <h1>Buscador de gifs</h1>
-        <p> Descubre y comparte el gif perfecto</p>
-      </div>
-      {/*Search */}
-    <div className="search-container">
-      <input type="text"  placeholder="Buscar gifs"/>
-      <button>Buscar</button>
-    </div>
-      {/*Busquedas previdas */}
-    <div className="previous-searches">
-      <h2>Busquedas previas</h2>
-      <ul className="previous-searches-list">
-        <li>One puch man </li>
-        <li>Goku</li>
-        <li>Saitama</li>
-      </ul>
-    </div>
+      <CustomHeader title="Buscador de gifs" description="Descubre y comparte el gif perfecto"></CustomHeader>
+    <SearchBar placeholder="Busca lo que quieras"></SearchBar>
+   <PreviousSearches searches={['goku','saitama','vegueta']}></PreviousSearches>
+
     {/*Gifs*/}
-      <div className="gifs-container">
-          {
-            mockGifs.map((gif)=>(
-                <div key={gif.id} className="gif-card">
-                    <img src={gif.url} alt={gif.title} />
-                    <h3>{gif.title}</h3>
-                    <p>
-                      {gif.width}x{gif.height}(1.5mb)
-                    </p>
-                </div>
-                )
-            )
-          }
-      </div>
+    <GifsList gifs={mockGifs}></GifsList>
     </>
 
 
---
+++ src/gifs/PreviousSearches.tsx
@@ -0,0 +1,15 @@
+interface Props{
+    searches: string[]
+}
+export const PreviousSearches= ({searches}:Props) => {
+    return (
+        <div className="previous-searches">
+            <h2>Busquedas previas</h2>
+            <ul className="previous-searches-list">
+                {
+                searches.map(term=>(
+                    <li key={term}>{term}</li>
+                ))}
+            </ul>
+        </div>)
+}
